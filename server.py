21a5c6b871b61ba419b3b2a50fe910f9bdf22951
koolbasov <kolbasov.anton@gmail.com>
2024-06-04 13:16:23 +0300

    Added html templates, bootstrap, news parser

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
+    title = "Новости Python"
     weather = weather_by_city("Saint-Petersburg,Russia")
-    if weather:
-        return (f"Температура: {weather['temp_C']}, "
-                f"ощущается как {weather['FeelsLikeC']}")
-    else:
-        return f"Сервис погоды временно недоступен"
+    news_list = get_python_news()
+    return render_template("index.html", page_title=title, weather=weather, news_list=news_list)
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -1,24 +1,31 @@
 import requests
 import settings
 
+
 def weather_by_city(city_name):
-    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
-    params = {'key': settings.API_KEY,
-              'q': city_name,
-              'format': 'json',
-              'num_of_days': 1,
-              'lang': 'ru'
-              }
-    result = requests.get(weather_url, params=params)
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except (IndexError, TypeError):
-                return False
+    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    params = {
+        "key": settings.API_KEY,
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 1,
+        "lang": "ru",
+    }
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if "data" in weather:
+            if "current_condition" in weather["data"]:
+                try:
+                    return weather["data"]["current_condition"][0]
+                except (IndexError, TypeError):
+                    return False
+    except (requests.RequestException, ValueError):
+        print("Сетевая ошибка")
+        return False
     return False
 
 
 if __name__ == "__main__":
-    print(weather_by_city('Saint_Petersburg,Russia'))
+    print(weather_by_city("Saint_Petersburg,Russia"))
