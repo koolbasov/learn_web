85596f5a29e8af42dce7bac42c9473e0ab29e42a
koolbasov <kolbasov.anton@gmail.com>
2024-06-25 15:59:20 +0300

    Переписали проект с использованием blueprints. Добавили меню и декоратор @admin_required

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -3,14 +3,16 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
-from webapp.model import db, News
+from webapp.db import db
+from webapp.news.models import News
+
 
 def get_html(url):
     try:
         result = requests.get(url)
         result.raise_for_status()
         return result.text
-    except(requests.RequestException, ValueError):
+    except (requests.RequestException, ValueError):
         print("Network error")
         return False
 
